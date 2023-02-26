ba71d5e783731e91d755625990c5b41697930dbc
Monica Pajon <moniestudio161@gmail.com>
2023-02-26 00:45:05 -0300

    navbar creation

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,10 +1,11 @@
+import { Navbar } from "../Navbar"
 import "./styles.scss"
 
 const Header = () => {
 
     return (
         <header className="main-header">
-            Este es el header
+           <Navbar />
         </header>
     )
 }
---
+++ src/components/layout/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import { Navbar as NavbarBTS, Container, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+
+    return (
+        <NavbarBTS bg="light" expand="lg">
+            <Container fluid>
+                <NavbarBTS.Brand href="#home">moviesApp</NavbarBTS.Brand>
+                <NavbarBTS.Toggle aria-controls="basic-navbar-nav" />
+                <NavbarBTS.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                        <NavLink className="nav-link" to="/login">Login</NavLink>
+                        <NavLink className="nav-link" to="/signup">Registro</NavLink>
+                        <NavLink className="nav-link" to="/categories">Categorías</NavLink>
+                        <NavLink className="nav-link" to="/users">Usuarios</NavLink>
+                    </Nav>
+                </NavbarBTS.Collapse>
+            </Container>
+        </NavbarBTS>
+    )
+}
+
+export { Navbar }
